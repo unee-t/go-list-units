458655d156447a6313ed75bc64bea8efb21d2381
Kai Hendry <kai.hendry@unee-t.com>
2018-10-31 14:00:30 +0800

    Refactor to extract db connection

--- main.go
+++ main.go
@@ -1,30 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
 	"os"
 
+	"github.com/apex/log"
 	jsonhandler "github.com/apex/log/handlers/json"
 	"github.com/apex/log/handlers/text"
 	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/unee-t/env"
-
-	"github.com/apex/log"
-
-	"database/sql"
-
-	_ "github.com/go-sql-driver/mysql"
 )
 
 type unit struct {
 	Id          int
 	Name        string
 	Description template.HTML
+}
+
+type handler struct {
+	DSN            string // e.g. "bugzilla:secret@tcp(auroradb.dev.unee-t.com:3306)/bugzilla?multiStatements=true&sql_mode=TRADITIONAL"
+	APIAccessToken string // e.g. O8I9svDTizOfLfdVA5ri
+	db             *sql.DB
+	Code           env.EnvCode
 }
 
 func init() {
@@ -36,47 +40,29 @@ func init() {
 }
 
 func main() {
+
+	h, err := New()
+	if err != nil {
+		log.WithError(err).Fatal("error setting configuration")
+		return
+	}
+
+	defer h.db.Close()
+
 	addr := ":" + os.Getenv("PORT")
 	app := mux.NewRouter()
-	app.HandleFunc("/", listjson).Methods("GET").Headers("Accept", "application/json")
-	app.HandleFunc("/", listhtml).Methods("GET")
+	app.HandleFunc("/", h.listjson).Methods("GET").Headers("Accept", "application/json")
+	app.HandleFunc("/", h.listhtml).Methods("GET")
+	app.HandleFunc("/metrics", h.prometheus)
 	if err := http.ListenAndServe(addr, app); err != nil {
 		log.WithError(err).Fatal("error listening")
 	}
 
 }
 
-func getUnits() (units []unit, err error) {
+func (h handler) getUnits() (units []unit, err error) {
 
-	cfg, err := external.LoadDefaultAWSConfig(external.WithSharedConfigProfile("uneet-dev"))
-	if err != nil {
-		log.WithError(err).Fatal("setting up credentials")
-		return
-	}
-	cfg.Region = endpoints.ApSoutheast1RegionID
-	e, err := env.New(cfg)
-	if err != nil {
-		log.WithError(err).Warn("error getting unee-t env")
-	}
-
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:3306)/bugzilla?multiStatements=true&sql_mode=TRADITIONAL",
-		e.GetSecret("MYSQL_USER"),
-		e.GetSecret("MYSQL_PASSWORD"),
-		e.Udomain("auroradb")))
-	if err != nil {
-		return units, err
-	}
-
-	defer db.Close()
-
-	// Open doesn't open a connection. Validate DSN data:
-	err = db.Ping()
-	if err != nil {
-		log.WithError(err).Error("failed to open database")
-		return units, err
-	}
-
-	rows, err := db.Query("select id, name, description from products")
+	rows, err := h.db.Query("select id, name, description from products")
 	if err != nil {
 		log.WithError(err).Error("failed to open database")
 		return units, err
@@ -106,13 +92,13 @@ func getUnits() (units []unit, err error) {
 
 }
 
-func listhtml(w http.ResponseWriter, r *http.Request) {
+func (h handler) listhtml(w http.ResponseWriter, r *http.Request) {
 
 	if os.Getenv("UP_STAGE") != "production" {
 		w.Header().Set("X-Robots-Tag", "none")
 	}
 
-	units, err := getUnits()
+	units, err := h.getUnits()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -123,13 +109,13 @@ func listhtml(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func listjson(w http.ResponseWriter, r *http.Request) {
+func (h handler) listjson(w http.ResponseWriter, r *http.Request) {
 
 	if os.Getenv("UP_STAGE") != "production" {
 		w.Header().Set("X-Robots-Tag", "none")
 	}
 
-	units, err := getUnits()
+	units, err := h.getUnits()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -137,5 +123,51 @@ func listjson(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(units)
+
+}
+
+func (h handler) prometheus(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "# HELP count_total shows the in-memory count, which will get reset in the event of the lambda going cold or scaling.\n# TYPE count_total counter\ncount_total")
+}
+
+// New setups the configuration assuming various parameters have been setup in the AWS account
+func New() (h handler, err error) {
+
+	cfg, err := external.LoadDefaultAWSConfig(external.WithSharedConfigProfile("uneet-dev"))
+	if err != nil {
+		log.WithError(err).Fatal("setting up credentials")
+		return
+	}
+	cfg.Region = endpoints.ApSoutheast1RegionID
+	e, err := env.New(cfg)
+	if err != nil {
+		log.WithError(err).Warn("error getting AWS unee-t env")
+	}
+
+	var mysqlhost string
+	val, ok := os.LookupEnv("MYSQL_HOST")
+	if ok {
+		log.Infof("MYSQL_HOST overridden by local env: %s", val)
+		mysqlhost = val
+	} else {
+		mysqlhost = e.Udomain("auroradb")
+	}
+
+	h = handler{
+		DSN: fmt.Sprintf("%s:%s@tcp(%s:3306)/bugzilla?multiStatements=true&sql_mode=TRADITIONAL",
+			e.GetSecret("MYSQL_USER"),
+			e.GetSecret("MYSQL_PASSWORD"),
+			mysqlhost),
+		APIAccessToken: e.GetSecret("API_ACCESS_TOKEN"),
+		Code:           e.Code,
+	}
+
+	h.db, err = sql.Open("mysql", h.DSN)
+	if err != nil {
+		log.WithError(err).Fatal("error opening database")
+		return
+	}
+
+	return
 
 }
